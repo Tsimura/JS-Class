dcaca2c1b293ef40af2a96920381fc49497e313b
ElenaStepanchuk <podolyak365@gmail.com>
2021-09-18 11:09:39 +0300

    2 task

--- js/module2.js
+++ js/module2.js
@@ -4,9 +4,39 @@
 //Удалите первый элемент массива и выведите его в консоль.
 //Вставьте «Рэп» и «Регги» в начало массива.
 
-const styles = ["Джаз", "Блюз"];
-styles.push("Рок-н-ролл");
-styles.splice(1, 1, "Классика");
-console.log(styles.shift())
-styles.unshift("Рэп", "Регги");
-console.log(styles);
+// const styles = ["Джаз", "Блюз"];
+// styles.push("Рок-н-ролл");
+// styles.splice(1, 1, "Классика");
+// console.log(styles.shift())
+// styles.unshift("Рэп", "Регги");
+// console.log(styles);
+
+
+//Напиши функцию min(a, b), которая возвращает
+//меньшее из чисел a, b
+//нужно добавить проверку, что функция получает числа
+
+// function min(a, b) {
+//     let c = 0;
+    
+//     return Math.min(a, b);
+// }
+// console.log(min(3, 5));
+
+// function min(a, b) {
+//     if (a > b) {
+//         return b;
+//     }
+
+//     return a;
+// }
+// console.log(min(3, 5));
+
+// function min(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         return 'передали не число';
+//     }
+// // console.log (typeof a)
+//     return a > b ? b : a;
+// }
+// console.log(min(3, 5));
